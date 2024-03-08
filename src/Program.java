5d45b5de72ac14212b35ca5e67f6c53c776fd495
Rafael Guilherme <rafael_guilherme@hotmail.com>
2024-03-08 11:51:49 -0300

    feat: insert data in to table

--- src/Program.java
+++ src/Program.java
@@ -1,36 +1,33 @@
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
 public class Program {
     public static void main(String[] args) {
 
-        // System.out.println("MySQL JDBC Connection Testing");
-        // Connection conn = null;
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
 
-        // try {
-        // Class.forName("com.mysql.cj.jdbc.Driver");
-        // String username = "developer";
-        // String password = "1234567";
-        // String url = "jdbc:mysql://localhost:3308/jdbcdb";
-        // conn = DriverManager.getConnection(url, username, password);
-        // System.out.println("Database connection established...");
-        // } catch (Exception ex) {
-        // System.err.println("Cannot connect to database server");
-        // ex.printStackTrace();
-        // } finally {
-        // if (conn != null) {
-        // try {
-        // System.out.println("Let's terminate connection");
-        // conn.close();
-        // } catch (Exception ex) {
-        // System.err.println("Cannot connect to database server");
-        // ex.printStackTrace();
-        // }
-        // }
-        // }
+        try {
+            conn = DB.getConnection();
 
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+            st = conn.createStatement();
+
+            rs = st.executeQuery("select * from department");
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
     }
 }
